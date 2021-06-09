3213181cbb8854b988fedef2865bf567f46c89de
ajibade3210 <ajibade69th@gmail.com>
2021-06-09 02:46:44 +0100

    vuex app for adopting pet

---
+++ src/data/cats.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'colltuxedaie',
+    species: 'cats',
+    gender: 'male',
+    age: 28,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'sweet kity. proud and picky.'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    species: 'cats',
+    gender: 'female',
+    age: 28,
+    color: 'orange/white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'gabby',
+    breed: 'tabby',
+    species: 'cats',
+    gender: 'female',
+    age: 28,
+    color: 'orange/white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Roger',
+    breed: 'afghan',
+    species: 'cats',
+    gender: 'male',
+    age: 19,
+    color: 'gray',
+    weight: '15',
+    location: 'threed',
+    notes: 'Doesnt Bite again'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    species: 'dogs',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competiton.'
+  },
+  {
+    name: 'Tommy',
+    breed: 'mut',
+    species: 'dogs',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: '37',
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'danny',
+    breed: 'mut',
+    species: 'dogs',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: '37',
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'John',
+    breed: 'afghan hound',
+    species: 'dogs',
+    gender: 'male',
+    age: 9,
+    color: 'graye',
+    weight: '68',
+    location: 'threed',
+    notes: 'Doesnt Bite'
+  }
+]
